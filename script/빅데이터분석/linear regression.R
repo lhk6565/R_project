35dbe42396b956f24d41447813cba86964d90585
HyungKwon Lee <lhk6565@naver.com>
2022-04-11 23:13:02 +0900

    study linear regression

---
+++ script/빅데이터분석/linear regression.R
@@ -0,0 +1,17 @@
+
+data = iris
+
+fix(data)
+
+summary(data)
+
+fit = lm(data$Petal.Width~data$Sepal.Length+data$Sepal.Width+data$Petal.Length, data=data)
+summary(fit)
+
+### hat matrix
+
+ones = matrix(1, nrow=nrow(data), ncol=1)
+x = as.matrix(iris[,1:3])
+y = as.matrix(iris[,4])
+x = cbind(ones,x)
+solve(t(x)%*%x) %*% t(x) %*% y # hat matrix
